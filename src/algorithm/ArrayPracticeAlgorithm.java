53c2cdac2f44fc75d97a287b840f73e5e127d5b2
DavidLee <78alswo79@gmail.com>
2024-12-23 20:50:07 +0900

    Random 난수로 최솟값, 합계값, 평균값, 5이상 20이하 난수사이의 가장 큰 값구하기 알고리즘 연습

---
+++ src/algorithm/ArrayPracticeAlgorithm.java
@@ -0,0 +1,105 @@
+package algorithm;
+
+import java.util.Random;
+
+public class ArrayPracticeAlgorithm {
+	private static void minof() {
+		 Random random = new Random();
+		 random.setSeed(System.currentTimeMillis()); 			// seed 생성
+		 
+		 int size = 7;
+		 int a[] = new int[size];
+		 
+		 for (int i = 0; i < size; i++) {
+			 a[i] = random.nextInt(101);  						// 0 ~ 101사이의 난수 생성.
+		 }
+		 
+		 int min = 0;
+		 min = a[0];
+		 for (int i = 1; i < a.length; i++) {
+			 if (a[i] < min) min = a[i];
+		 }
+		 System.out.println("최솟값은 ????" + min);
+	 }
+	 
+	 private static void sumof() {
+		 Random random = new Random();
+		 random.setSeed(System.currentTimeMillis()); 			// seed 생성
+		 
+		 // 배열 초기화.
+		 int size = 7;
+		 int a[] = new int[size];
+		 
+		 for (int i = 0; i < size; i++) {
+			 a[i] = random.nextInt(101);  						// 0 ~ 101사이의 난수 생성.
+			 //System.out.println("생성된 난수들" + a[i]);
+		 }
+		 
+		 int sum = 0;
+		 sum = a[0];
+		 for (int i = 1; i < a.length; i++) {
+			 sum += a[i];
+		 }
+		 System.out.println("생성된 난수들의 합계는 ????" + sum);
+	 }
+	 
+	 private static void aveof() {
+		 Random random = new Random();
+		 random.setSeed(System.currentTimeMillis()); 			// seed 생성
+		 
+		 // 배열 초기화.
+		 int size = 7;
+		 int a[] = new int[size];
+		 
+		 for (int i = 0; i < size; i++) {
+			 a[i] = random.nextInt(101);  						// 0 ~ 101사이의 난수 생성.
+			 //System.out.println("생성된 난수들" + a[i]);
+		 }
+		 
+		 int avg = 0;
+		 int sum = a[0];
+		 for (int i = 1; i < a.length; i++) {
+			 sum += a[i];
+		 }
+		 avg = sum / a.length;
+		 System.out.println("생성된 난수들의 평균는 ????" + avg);
+	 }
+	 
+	 private static void highTall () {
+		 Random rd = new Random();
+		 rd.setSeed(System.currentTimeMillis());
+		 
+		 // 5이상 20이하의 난수 생성.
+		 int rdInt = 0;
+		 System.out.println("5이상 20이하의 난수들" + rdInt);
+		 int size = 20 - 5;
+		 int a[] = new int[size];
+		 
+		 // 배열 초기화
+		 for (int i = 0; i < a.length; i++) {
+			 rdInt = rd.nextInt(16)+5;
+			 a[i] = rdInt;
+		 }
+		 
+		 int max = a[0];
+		 for (int i = 1; i < a.length; i++) {
+			 if (a[i] > max) max = a[i];
+		 }
+		 System.out.println("최대값은 ????" + max);
+	 }
+
+	public static void main(String[] args) {
+		// 최솟값 구하기
+		minof();
+		// 합계 구하기
+		sumof();
+		// 평균 구하기
+		aveof();
+		// 사람 수 5 ~ 21미만의 난수로 가장 큰 키 구하기
+		highTall();
+		// TODO any_reverse()작성하기
+
+	}
+	 
+
+}
